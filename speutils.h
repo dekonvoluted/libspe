6cbee7cc0bb3f66ac873522aa55e3076108b069d
Karthik Periagaram <karthik.periagaram+git@gmail.com>
2013-01-27 22:22:47 -0800

    Working demo now available for testing
    * demo builds and executes

--- speutils.h
+++ speutils.h
@@ -5,24 +5,27 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // libspe is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with libspe.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "spe.h"
+// Utilities to work with spe files
+// Needs <vector>
+// Needs <string>
+// Needs "spe.h"
 
-using namespace std;
+#ifndef SPEUTILS_H
+#define SPEUTILS_H
 
-int main()
-{
-  speFile* image = new speFile( "image.spe" );
-  
-  delete image;
-  
-  return 0;
-}
+#include <vector>
+#include <string>
+
+std::vector<std::string> lsspe( std::string ); // List spe files in a directory
+
+#endif
+
--- src/speutils.cpp
+++ src/speutils.cpp
@@ -5,24 +5,42 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // libspe is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with libspe.  If not, see <http://www.gnu.org/licenses/>.
 
+#include <ostream>
+#include <dirent.h>
+#include <fnmatch.h>
+
 #include "spe.h"
+#include "speutils.h"
 
-using namespace std;
-
-int main()
+int filter( const struct dirent* file )
 {
-  speFile* image = new speFile( "image.spe" );
-  
-  delete image;
-  
-  return 0;
+  return !fnmatch( "*.spe", file->d_name, FNM_CASEFOLD );
 }
+
+std::vector<std::string> lsspe( const std::string directory )
+{
+  std::vector<std::string> filePaths;
+
+  if( !opendir( directory.c_str() ) ) return filePaths; // Invalid directory
+
+  struct dirent** files;
+  int numFiles = scandir( directory.c_str(), &files, filter, alphasort );
+
+  if( numFiles == 0 ) return filePaths; // No files found
+
+  for( int i = 0; i < numFiles; i++ ) {
+    filePaths.push_back( ( directory.find_last_of( '/' ) + 1 == directory.size() )? directory + std::string( files[ i ]->d_name ) : directory + "/" + std::string( files[ i ]->d_name ) );
+  }
+
+  return filePaths;
+}
+
